8f3540b4963f16ecf40d137c0963ffc41eb2b5aa
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-07-23 23:23:25 +0300

    add basic math operations in js

---
+++ jscript.js
@@ -0,0 +1,23 @@
+// Addition
+function add(a, b) {
+  sum = a + b;
+  return sum;
+};
+
+//Subtraction
+function subtract(a, b) {
+  sub = a - b;
+  return sub;
+};
+
+//Multiplication
+function multiply(a, b) {
+  multi = a * b;
+  return multi;
+};
+
+//Division
+function divide(a, b) {
+  divide = a / b;
+  return divide;
+};

4e819bdc3a8d7fe89eee09cc2bdb9cb4b8a1abc6
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-08-06 23:18:18 +0300

    change traditional functions to arrow functions

--- jscript.js
+++ jscript.js
@@ -4,7 +4,7 @@ let operation = "";
 let operator = "";
 let variable1;
 let variable2;
-let result = document.createElement("div")
+const result = document.createElement("div");
 // Addition
 function add(variable1, variable2) {
   sum = variable1 + variable2;
@@ -42,45 +42,58 @@ function operate(variable1, variable2, operator) {
       return multiply(variable1, variable2);
       break;
     case "division":
-      return divide(variable1, variable2)
+      return divide(variable1, variable2);
       break;
   };
 };
 
-document.getElementById("calculator-container").addEventListener("click", function(e) {
+document.getElementById("calculator-container").addEventListener("click", (e) => {
   if(e.target.className =="numbers" && e.target.nodeName =="BUTTON" && operator=="") {
-      variable = e.target.textContent
-      field.value += variable
-    }
+      variable = e.target.textContent;
+      field.value += variable;
+    };
     if(e.target.className =="numbers" && e.target.nodeName =="BUTTON" && !operator=="") {
-      variable = e.target.textContent
-      field.value += variable
-    }
+      variable = e.target.textContent;
+      field.value += variable;
+    };
 });
-document.getElementById("calculator-container").addEventListener("click", function(e) {
+
+document.getElementById("calculator-container").addEventListener("click", (e) => {
   if(e.target.className =="operation" && e.target.nodeName =="BUTTON" && operator=="") {
-      operator = e.target.id
-      variable1= parseInt(field.value)
-      result.textContent = variable1
-      results.appendChild(result)
-      field.value = ""
+      operator = e.target.id;
+      variable1= parseInt(field.value);
+      result.textContent = variable1;
+      results.appendChild(result);
+      field.value = "";
       return;
-    }
+    };
   if(e.target.className =="operation" && e.target.nodeName =="BUTTON" && !operator=="") {
-    variable2 = parseInt(field.value)
-    result.textContent = operate(variable1, variable2, operator)
-    results.appendChild(result)
-    field.value = ""
-    variable1 = parseInt(results.textContent)
+    variable2 = parseInt(field.value);
+    result.textContent = operate(variable1, variable2, operator);
+    results.appendChild(result);
+    field.value = "";
+    variable1 = parseInt(results.textContent);
     operator = e.target.id;
-    }
+    };
 });
-const equation = document.querySelector("#equation")
-equation.addEventListener("click", function(e) {
-  variable2 = parseInt(field.value)
-  result.textContent = operate(variable1, variable2, operator)
-  results.appendChild(result)
-  field.value = ""
-  variable1 = parseInt(results.textContent)
+
+const equation = document.querySelector("#equation");
+equation.addEventListener("click", () => {
+  variable2 = parseInt(field.value);
+  operationResult = operate(variable1, variable2, operator);
+  result.textContent = operationResult;
+  results.appendChild(result);
+  field.value = "";
+  variable1 = parseInt(results.textContent);
   operator = "";
-})
+});
+
+const clearData = document.querySelector("#clear");
+clearData.addEventListener("click", () => {
+  field.value = "";
+  results.textContent = "";
+  variable1 = "";
+  variable2 = "";
+  operator = "";
+}
+)
